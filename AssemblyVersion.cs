c11c0baa26cdbdf80834b26685c931e21cfa7d72
Clayel <videogamer0907@gmail.com>
2025-06-26 20:03:23 -0400

    add KSPAssembly

--- AssemblyVersion.cs
+++ AssemblyVersion.cs
@@ -1,6 +1,7 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyFileVersion("2.0.0.0")] // only change this one
+[assembly: AssemblyVersion("2.0.0.0")] // only change this one occasionally
+[assembly: AssemblyFileVersion("2.0.0.0")]
+[assembly: KSPAssembly("LunarTransferPlanner", 2, 0, 0)]
 [assembly: KSPAssemblyDependency("ClickThroughBlocker", 1, 0)]
 [assembly: KSPAssemblyDependency("ToolbarController", 1, 0)]
